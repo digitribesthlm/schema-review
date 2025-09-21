16c1fbf101016ee3701aaa0b24eea8db67e9944f
digitribesthlm <51318199+digitribesthlm@users.noreply.github.com>
2025-09-21 11:32:36 -0400

    Remove problematic redirect from next.config.js
    - Removed redirect that was interfering with API routes
    - This should fix the 404 errors on all API endpoints
    - API routes should now work properly

--- next.config.js
+++ next.config.js
@@ -1,22 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/login',
-        permanent: false,
-        has: [
-          {
-            type: 'header',
-            key: 'accept',
-            value: '(?!.*application/json).*', // Don't redirect API calls
-          },
-        ],
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
